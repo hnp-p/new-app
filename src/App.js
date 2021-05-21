5c3692da0850e1ba996771ec0d3801f4bc0ab085
DuyDuy <duy.chau@manifera.com>
2021-05-21 14:12:20 +0700

    add new killing

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ function App() {
             I am killing myself working all the timeThere was a point where I
             was not eating, showering or doing anything else other than....
           </p>
+          <p>
+            I am killing myself working all the timeThere was a point where I
+            was not eating, showering or doing anything else other than....
+          </p>
         </div>
       </header>
     </div>
